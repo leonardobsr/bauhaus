89f8ece8fa97dcd6fe70c8f95ff3aba06f3056af
Leonardo Reis <leonardobsr@gmail.com>
2019-10-09 14:21:55 -0300

    start board setup

---
+++ Bauhaus Shared/Components/DotComponent.swift
@@ -0,0 +1,26 @@
+//
+//  DotComponent.swift
+//  Bauhaus
+//
+//  Created by Leonardo Reis on 08/10/19.
+//  Copyright © 2019 LeonardoBSR. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class DotComponent: GKComponent {
+    
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func update(deltaTime seconds: TimeInterval) {
+        super.update(deltaTime: seconds)
+    }
+    
+}
